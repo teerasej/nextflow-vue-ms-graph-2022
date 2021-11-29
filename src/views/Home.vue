f7c1d05b188a3fe80aae6e8cef7ab3372e3caebe
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-11-30 00:45:55 +0700

    set data and template for ready to use

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,45 +2,66 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Vue Graph</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <div v-if="!user">
+          <ion-button>Sign in</ion-button>
+        </div>
+        <div v-if="user">
+          <p>Hello, {{ user.name }}</p>
+          <ul>
+            <li v-for="item in messages" :key="item.id">
+              {{ item.subject }}
+            </li>
+          </ul>
+          <ion-button>Sign out</ion-button>
+        </div>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+  },
+
+  data: function () {
+    return {
+      user: {},
+      messages: [
+        { subject: "Learn JavaScript" },
+        { subject: "Learn Vue.js" },
+        { subject: "Build Something Awesome" },
+      ],
+    };
+  },
 });
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -56,9 +77,9 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
